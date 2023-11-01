f145366188305543b5b19ec65b5d857611d022ef
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-11-01 16:04:58 -0700

    made navbar stcicky and change the color on scroll and added smooth scroll as well

--- components/Shared/Navigations/Navbar.tsx
+++ components/Shared/Navigations/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ComponentWrapper from "../Wrappers/ComponentWrapper";
 import Link from "next/link";
 import Drawer from "@mui/material/Drawer";
@@ -8,9 +8,42 @@ import { RxCross1 } from "react-icons/rx";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  // Function to handle the scroll event
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
+
+    // Remove the scroll event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   const toggleDrawer = () => {
     setIsOpen((prevvalue: boolean) => !prevvalue);
   };
+  // function fpr smooth scrolling
+  let navLinks = document.querySelectorAll("nav a");
+
+  for (const link of navLinks) {
+    link.addEventListener("click", smoothScroll);
+  }
+
+  function smoothScroll(this: any, event: any) {
+    event.preventDefault();
+
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
   const menu = [
     {
       name: "Features",
@@ -26,7 +59,7 @@ const Navbar = () => {
     },
     {
       name: "Pricing",
-      route: "#pricing",
+      route: "#howitworks",
     },
     {
       name: "FAQS",
@@ -34,8 +67,12 @@ const Navbar = () => {
     },
   ];
   return (
-    <ComponentWrapper style="z-50">
-      <nav className="w-full h-[70px] flex items-center justify-between bg-transparent z-50">
+    <ComponentWrapper
+      style={`z-50 ${
+        isScrolled ? "bg-white-main" : "bg-transparent"
+      } fixed w-full duration-300`}
+    >
+      <div className="w-full h-[70px] flex items-center justify-between z-50">
         <Link
           href={"/"}
           className="text-brand-main text-xl font-quicksand font-semibold"
@@ -43,7 +80,7 @@ const Navbar = () => {
           {" "}
           LOGO
         </Link>
-        <div className="h-full lg:flex hidden items-center justify-center gap-8 z-10">
+        <nav className="h-full lg:flex hidden items-center justify-center gap-8 z-10">
           {menu.map((item: any, index: number) => (
             <Link
               key={index}
@@ -53,17 +90,19 @@ const Navbar = () => {
               {item.name}
             </Link>
           ))}
-        </div>
+        </nav>
         <Link
           href={"/"}
-          className="px-[44px] py-3 z-10 lg:flex hidden rounded bg-white-main text-base text-brand-main font-semibold border border-white-main"
+          className={`px-[44px] py-3 z-10 lg:flex hidden rounded bg-white-main text-base text-brand-main font-semibold border ${
+            isScrolled ? "border-black-main" : "border-white-main"
+          } `}
         >
           Get Started
         </Link>
         <button onClick={toggleDrawer} className="inline-block lg:hidden z-10">
           <RiMenu3Line className="text-2xl text-black-main" />
         </button>
-      </nav>
+      </div>
       <Drawer
         anchor={"right"}
         open={isOpen}
@@ -82,16 +121,19 @@ const Navbar = () => {
               <RxCross1 className="text-2xl text-brand-main" />
             </button>
           </div>
-          {menu.map((item: any, index: number) => (
-            <Link
-              key={index}
-              href={item.route}
-              onClick={toggleDrawer}
-              className="text-base text-black-off hover:text-brand-main hover:duration-200 font-semibold"
-            >
-              {item.name}
-            </Link>
-          ))}
+          <nav className="w-full flex flex-col items-center justify-start gap-8">
+            {menu.map((item: any, index: number) => (
+              <Link
+                key={index}
+                href={item.route}
+                onClick={toggleDrawer}
+                className="text-base text-black-off hover:text-brand-main hover:duration-200 font-semibold"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </nav>
+
           <Link
             href={"/"}
             onClick={toggleDrawer}
